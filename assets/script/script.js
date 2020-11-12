543f73e861c02bc5db7bd568210998c3c27cf312
Isaac de Oliveira <isaacbyguaracity@gmail.com>
2020-11-12 14:13:44 -0300

    Adicionando paginas Blog e Contato

---
+++ assets/script/script.js
@@ -0,0 +1,18 @@
+//The core Firebase JS SDK is always required and must be listed first 
+
+//TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries 
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDNYFq9deSMpYJurNOt-YIG5yyIPHdYNLg",
+    authDomain: "incubadoraproject-2c334.firebaseapp.com",
+    databaseURL: "https://incubadoraproject-2c334.firebaseio.com",
+    projectId: "incubadoraproject-2c334",
+    storageBucket: "incubadoraproject-2c334.appspot.com",
+    messagingSenderId: "800653455988",
+    appId: "1:800653455988:web:16c30825b18d7155b05721"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
